9ee5740f3be18c2230545c9979e5de79eb233af1
antgreen13 <anthony.green5@proton.me>
2022-08-25 19:34:09 -0400

    Add Google oAuth and sign in page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SocialAuthService, GoogleLoginProvider, SocialUser  } from "@abacritt/angularx-social-login";
+import { CHARACTERS } from '../data/mock-characters';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user: SocialUser | undefined;
+  GoogleLoginProvider = GoogleLoginProvider;
 
-  constructor() { }
+  characterData = CHARACTERS;
+  displayedColumns: string[] = ['name', 'race', 'class'];
 
-  ngOnInit(): void {
+  constructor(private readonly authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  refreshGoogleToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
   }
 
 }
